a177c17bd8b34c5adfc2bd4a6fd7cc395ee9baca
Erkan <ErkanOzulu@users.noreply.github.com>
2023-06-18 15:25:58 +0300

    practices

--- src/InterviewQuestions/multiply.java
+++ src/InterviewQuestions/multiply.java
@@ -6,24 +6,28 @@ public class multiply {
     
 
     public static void main(String[] args) {
-arraysmultipl();
+
 
         int []k= {-1, 3, 4, 5, 3, 2};
 
-        System.out.println(Arrays.toString(k));
-        int[] arr=new int[k.length];
 
-        for (int i = 0; i < k.length; i++) {
-            arr[i]=k[i]*k[i];
+        multiply(k);
+        Arrays.sort(k);
+        System.out.println(Arrays.toString(k));
+
+    }
+
+    public static int[] multiply(int[] a){
+
+        int[] arr=new int[a.length];
+
+        for (int i = 0; i < a.length; i++) {
+            arr[i]=a[i]*a[i];
 
         }
-
         Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
-
-
+        return arr;
     }
 
-    private static void arraysmultipl() {
-    }
 }
